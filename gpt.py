2870cc43e62bc533341ace1e88f68d445647e85a
uxiost <uxio.st@gmail.com>
2023-01-24 21:34:15 +0000

    added dropout, scaled hyperparams

--- gpt.py
+++ gpt.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # parameters
-batch_size = 32  # number of independent sequences to run in parallel
-block_size = 8  # max context lenght of the sequences
+batch_size = 64  # number of independent sequences to run in parallel
+block_size = 256  # max context lenght of the sequences
 max_iters = 5000
 eval_interval = 500  # iterations between model evaluations
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200 # n of batches to evaluate performance (more batches -> more precise)
-n_embd = 32 # embedding dimension, can also be refered as C
+eval_iters = 200 # n of batches to evaluate performance (more batches -> more realistic eval)
+n_embd = 384 # embedding dimension, can also be refered as C
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -65,6 +68,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size,block_size))) # non-trainable parameters
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B,T,C = x.shape
@@ -74,6 +78,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * self.head_size ** -0.5
         wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf'))  # tril[:T,:T] is used for compatibility with generate method
         wei = F.softmax(wei, dim=-1) # B, T, T
+        wei = self.dropout(wei)
         # agregate values
         v = self.value(x) # B, T, head_size
         out = wei @ v # (B, T, T) @ (B, T, head_size) ->B, T, head_size
@@ -85,10 +90,11 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim = -1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
 
 class FeedForward(nn.Module):
@@ -100,6 +106,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
         )
 
     def forward(self, x):
@@ -122,20 +129,16 @@ class Block(nn.Module):
         x = x + self.ffwd(self.ln2(x))
         return x
 
-# simple bigram model
-class BigramLanguageModel(nn.Module):
+# GPT model
+class GPTLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         # each token reads logits for the next one from this lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            nn.LayerNorm(n_embd)
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -196,4 +199,7 @@ for iter in range(max_iters):
 # generate using model
 # init context with first token (\n)
 idx = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(idx, 500)[0].tolist()))
+output = decode(m.generate(idx, 10000)[0].tolist())
+with open('output.txt', 'w') as f:
+    f.write(output)
+print(output[:1000])
